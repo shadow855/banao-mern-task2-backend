e3508da6666c493607cd3b5e28e0c3cb3f9718e1
shadow855 <raiyanmushtaque@gmail.com>
2024-06-21 17:07:53 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json()); // to accept json data
 
 app.use(cors({
-    origin: ["http://localhost:3000", ""]
+    origin: ["http://localhost:3000", "https://banao-mern-task2-frontend.onrender.com"]
 }));
 
 
